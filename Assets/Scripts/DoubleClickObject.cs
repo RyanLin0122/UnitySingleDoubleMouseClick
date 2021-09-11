6faebe8ade3c82809cf28812c2e4683071ba1a2a
ryan.lin.hex@gmail.com <ryan.lin.hex@gmail.com>
2021-09-11 22:00:26 +0800

    Upload the project

---
+++ Assets/Scripts/DoubleClickObject.cs
@@ -0,0 +1,85 @@
+using UnityEngine;
+using UnityEngine.Events;
+using UnityEngine.EventSystems;
+using System;
+
+public class DoubleClickObject : MonoBehaviour, IPointerClickHandler
+{
+    public int ClickCount = 0;
+    public float DelayTime = 0.3f;
+    public bool IsLock = false;
+    public MouseButtonType MouseButtonType = MouseButtonType.Left;
+
+    [Serializable]
+    private class Event : UnityEvent { };
+    [SerializeField]
+    private Event SingleClickEvents = new Event();
+    [SerializeField]
+    private Event DoubleClickEvents = new Event();
+
+    public void OnPointerClick(PointerEventData eventData)
+    {
+        switch (MouseButtonType)
+        {
+            case MouseButtonType.Left:
+                if (eventData.pointerId != -1) return;
+                break;
+            case MouseButtonType.Right:
+                if (eventData.pointerId != -2) return;
+                break;
+            case MouseButtonType.Middle:
+                if (eventData.pointerId != -3) return;
+                break;
+        }
+        if (ClickCount == 0)
+        {
+            if (!IsLock)
+            {
+                Invoke("Timer", DelayTime);
+            }
+        }
+        ClickCount++;
+    }
+    private void Timer()
+    {
+        if (ClickCount >= 2)
+        {
+            IsLock = true;
+            Invoke("TimerAfterDoubleClick", DelayTime);
+            OnDoubleClick();
+        }
+        else if (ClickCount == 1)
+        {
+            OnClick();
+        }
+        ClickCount = 0;
+    }
+    private void TimerAfterDoubleClick()
+    {
+        IsLock = false;
+        ClickCount = 0;
+    }
+
+    /// <summary>
+    /// Single click event function
+    /// </summary>
+    protected virtual void OnClick()
+    {
+        SingleClickEvents.Invoke();
+    }
+
+    /// <summary>
+    /// Double click event function
+    /// </summary>
+    protected virtual void OnDoubleClick()
+    {
+        DoubleClickEvents.Invoke();
+    }
+}
+
+public enum MouseButtonType
+{
+    Left,
+    Middle,
+    Right
+}
---
+++ Assets/Scripts/TestFunction.cs
@@ -0,0 +1,15 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TestFunction : MonoBehaviour
+{
+    public void SingleClickTestFunction()
+    {
+        Debug.Log("Test single click");
+    }
+    public void DoubleClickTestFunction()
+    {
+        Debug.Log("Test double click");
+    }
+}
